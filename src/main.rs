1e5fc514b2161edf42f5063ebb2b6292c3730678
Brian Amsler <manof2words@yahoo.com>
2022-10-19 19:08:29 -0700

    finished some basic dictionary functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use crate::words::{Word, Dictionary};
+
+mod words;
+
 fn main() {
-    println!("Hello, world!");
+    let test = ["a", "asdf","b", "c", "e", "f", "g"];
+
+    let mut words = Vec::new();
+    words.reserve(test.len());
+
+    for w in test {
+        let word = Word { text: w.to_string(), definition: "".to_string(), pronunciation: None, obscurity: ((w.chars().nth(0).unwrap() as u32) - ('a' as u32) + 1) };
+
+        words.push(word);
+    }
+
+    let dict = Dictionary::create(words.into_boxed_slice(), words::ObscurityMode::Manual);
+    println!("dict: {:?}", dict);
+
+    let words = dict.get_words_leq_score(400);
+    print!("words: {:?}", words);
 }
