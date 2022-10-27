ed228cd6b975bb6298a3efdf1f17b0744be72a2a
Brian Amsler <manof2words@yahoo.com>
2022-10-26 18:22:40 -0700

    added user object; moved some stuff around

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,40 @@
 #![cfg_attr(debug_assertions, allow(dead_code))] 
 
+use crate::program::User;
 use std::{fs::File, collections::HashMap, rc::Rc, ops::Index};
-
-use chrono::Utc;
 
 use crate::{words::{Dictionary, Knowledge}};
 
 mod words;
 mod xml;
 mod constants;
+mod program;
+mod tools;
 
-struct DictMap {
-    map: HashMap<String, Rc<Dictionary>>
+fn gen_usr(dicts: &DictMap, filename: &str) -> User {
+    let jp_dict = &dicts["10,000 Most Common Japanese Words (kanjibox.net)"];
+
+    let mut kw = Knowledge::create(jp_dict.clone());
+
+    kw.randomize();
+
+    let mut user = User::create("guy".to_owned());
+    user.add_knowledge(kw);
+
+    let mut f = File::create(filename).unwrap();
+    user.save_to(&mut f).unwrap()
 }
 
-impl DictMap {
-    pub fn new() -> DictMap {
-        DictMap { map: HashMap::new() }
-    }
+fn load_usr(dicts: &DictMap, filename: &str) -> User {
+    let mut f = File::open(filename).unwrap();
 
-    pub fn insert(&mut self, dict: Rc<Dictionary>) -> Option<Rc<Dictionary>> {
-        self.map.insert(dict.get_title().to_owned(), dict)
-    }
-}
-
-impl Index<String> for DictMap {
-    type Output = Rc<Dictionary>;
-
-    fn index(&self, index: String) -> &Self::Output {
-        self.map.index(&index)
-    }
+    User::load_from(&mut f, dicts).unwrap()
 }
 
 fn main() {
     let mut jp_file = File::open("misc/japanese.dct").unwrap();
     let jp_dict = Rc::new(Dictionary::load_from(&mut jp_file).unwrap());
-    drop(jp_file);
 
     let mut dict_map = DictMap::new();
     dict_map.insert(jp_dict.clone());
-
-    let mut kw_file = File::open("misc/test.kw").unwrap();
-    let kw = Knowledge::load_from(&mut kw_file, &dict_map).unwrap();
-    drop(kw_file);
-
-    let word = jp_dict.find_word("お金".to_owned()).unwrap();
-    //kw.practice(word, 5);
-
-    let wk = kw.get_word_knowledge(word);
-
-    let time = wk.last_practice.unwrap().time();
-    let delta = Utc::now() - time;
-    let m = delta.num_seconds() / 60;
-    let s = delta.num_seconds() % 60;
-    println!("Time since last practice: {} minutes and {} seconds", m, s);
-
-    let mut f = File::create("misc/test.kw").unwrap();
-    kw.save_to(&mut f).unwrap();
 }
